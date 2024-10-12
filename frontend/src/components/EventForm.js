b5615d857d6511bc461909488c4346f845340e7a
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-13 02:28:26 +0300

    added authentication

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
+import { getAuthToken } from '../util/auth';
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -86,7 +87,8 @@ export default EventForm;
 export async function action({ request, params }) {
   const method = request.method;
   const data = await request.formData();
-
+  const token = getAuthToken();
+  
   const eventData = {
     title: data.get('title'),
     image: data.get('image'),
@@ -105,6 +107,7 @@ export async function action({ request, params }) {
     method: method,
     headers: {
       'Content-Type': 'application/json',
+       'Authorization': 'Bearer ' + token
     },
     body: JSON.stringify(eventData),
   });
--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -19,8 +19,7 @@ function ErrorPage() {
   }
 
   return (
-    <>
-      <MainNavigation />
+    <>     
       <PageContent title={title}>
         <p>{message}</p>
       </PageContent>
